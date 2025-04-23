58627ecea32607ef08c9f806a5565c178ebf4228
Mamta <mamtathakur11329111@gmail.com>
2025-04-23 22:04:57 +0530

    Minor change

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom'; // Import HashRouter
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router> {/* Wrap your App with HashRouter */}
+      <App />
+    </Router>
   </React.StrictMode>
 );
